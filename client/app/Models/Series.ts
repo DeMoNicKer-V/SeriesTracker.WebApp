a2fa5b698b650245520e6ec7b38922ee22517e6a
Виктор <v.shakov@list.ru>
2024-04-18 22:13:48 +0300

    I found out the difference between a class/interface and replaced the class with an interface.

--- client/app/Models/Series.ts
+++ client/app/Models/Series.ts
@@ -1,15 +1,16 @@
-class Series {
-    item1: item1 = new item1();
-    item2: item2 = new item2();
+interface Series {
+    item1: item1;
+    item2: item2;
 }
-class item1 {
-    id!: string;
-    title!: string;
-    description!: string;
-    currentEpisode!: number;
-    lastEpisode!: number;
-    releaseDate!: string;
+
+interface item1 {
+    id: string;
+    title: string;
+    description: string;
+    currentEpisode: number;
+    lastEpisode: number;
+    releaseDate: string;
 }
-class item2 {
-    count!: number | 0;
+interface item2 {
+    count: number;
 }
