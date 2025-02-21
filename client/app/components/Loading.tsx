be1e60da4284c9300d8389be50a422c24a06435b
Виктор <v.shakov@list.ru>
2025-02-21 18:33:41 +0300

    Unused/unnecessary components and styles have been removed.

--- client/app/components/Loading.tsx
+++ client/app/components/Loading.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Spin } from "antd";
-import { useLoading } from "./LoadingContext";
-import { LoadingOutlined } from "@ant-design/icons";
 
 interface Props {
     loading?: boolean;
